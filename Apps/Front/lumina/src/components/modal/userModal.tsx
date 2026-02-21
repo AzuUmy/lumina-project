59b4375f6fcc54e55e8b42c9b818c3b3e936a86b
AzuUmy <carlos_pherbis@hotmail.com>
2026-02-21 02:03:09 +0000

    :sparkles: added user modal

--- Apps/Front/lumina/src/components/modal/userModal.tsx
+++ Apps/Front/lumina/src/components/modal/userModal.tsx
@@ -1,8 +1,21 @@
 export function UserModal() {
+
+
+    const user = "Carlos Samuel"
+
   return (
-    <div className="p-6">
-      <h2 className="text-lg font-semibold">User context</h2>
-      <p className="mt-2 opacity-80">Theme-aware modal content placeholder.</p>
+    <div className="p-5 " >
+        <div className="flex items-center gap-3">
+             <div className="p-5 w-5 h-5 bg-blue-500 rounded-full"></div>
+        <div>{user}</div>
+        </div>
+        <div className="flex flex-col gap-3 mt-5 ml-13">
+              <a href="@">Profile</a>
+                <a href="@">Favorites</a>
+        <a href="@">History</a>
+        </div>
+       
     </div>
+
   );
 }
