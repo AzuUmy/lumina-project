269ea075152b2366e8dc3d885a1eaf344d788cb0
AzuUmy <carlos_pherbis@hotmail.com>
2026-03-14 18:33:48 +0000

    :lipstick: changed top menu style

--- Apps/Front/lumina/src/components/menu/topMenuComponent.tsx
+++ Apps/Front/lumina/src/components/menu/topMenuComponent.tsx
@@ -37,48 +37,51 @@ export function TopMenuComponent({
   }, [titleElement]);
 
   return (
-    <div className="flex gap-5">
-      <div className="fixed left-10 top-5  z-50">
-        <button 
-          type="button"
-          onClick={() => {
-            if (backOnClik) {
-              backOnClik();
-              return;
-            }
-            window.history.back();
-          }}
-          className=" top-0 left-10 mt-10 p-3 rounded-full flex justify-center backdrop-blur-md"
-          style={{
-            backgroundColor: "var(--backdrop)",
-            border: "1px solid var(--border)",
-          }}
-          aria-label="Go back"
-        >
-          <ArrowBackIosNewRounded />
-        </button>
-        <div
-          className={[
-            "fixed left-34 top-11.5",
-            showFloatingTitle && effectiveTitle
-              ? "opacity-100 translate-y-0"
-              : "opacity-0 translate-y-4 pointer-events-none",
-          ].join(" ")}
-        >
-          <div className="flex flex-row items-center gap-2">
-            <div
-              className="rounded-full px-4 py-2 text-sm font-semibold"
-              style={{
-                backgroundColor: "var(--backdrop)",
-                border: "1px solid var(--border)",
-              }}
-            >
-              {effectiveTitle}
-            </div>
-            <div>
-              <button className="bg-(--border) p-2 pl-3 pr-3 rounded-full">
-                Add to list
-              </button>
+    <div className="flex">
+      <div className="fixed left-0 top-0 z-50 w-screen p-10 ">
+        <div className="flex items-center justify-between bg-white/10 p-1 backdrop-blur-md rounded-full">
+          <button
+            type="button"
+            onClick={() => {
+              if (backOnClik) {
+                backOnClik();
+                return;
+              }
+              window.history.back();
+            }}
+            className=" top-0 left-10 p-3 rounded-full flex justify-center backdrop-blur-md"
+            style={{
+              backgroundColor: "var(--backdrop)",
+              border: "1px solid var(--border)",
+            }}
+            aria-label="Go back"
+          >
+            <ArrowBackIosNewRounded />
+          </button>
+          <div
+            className={[
+              showFloatingTitle && effectiveTitle
+                ? "opacity-100 translate-y-0"
+                : "opacity-0 translate-y-4 pointer-events-none",
+            ].join(" ")}
+          >
+            <div className="flex flex-row  items-center gap-2 p-1">
+              <div
+                className="rounded-full px-4 py-2 text-sm font-semibold"
+                style={{
+                  backgroundColor: "var(--backdrop)",
+                  border: "1px solid var(--border)",
+                }}
+              >
+                {effectiveTitle
+                  .slice(0, 19)
+                  .concat(effectiveTitle.length > 20 ? "..." : "")}
+              </div>
+              <div>
+                <button className="bg-(--border) p-2 pl-3 pr-3 rounded-full">
+                  Add to list
+                </button>
+              </div>
             </div>
           </div>
         </div>
